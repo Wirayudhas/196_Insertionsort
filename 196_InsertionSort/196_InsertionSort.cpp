7bd4615595fa3edb195ae74dce58c04a1dc00dc7
Wirayudhas <arturwirayuda@gmail.com>
2024-03-14 11:29:33 +0700

    Menambah perulangan For

--- 196_InsertionSort/196_InsertionSort.cpp
+++ 196_InsertionSort/196_InsertionSort.cpp
@@ -22,3 +22,11 @@ void input() {                  //prosedur input
 	cout << "====================" << endl;         // Membuat tampilan susunan data elemen Array
 	cout << "Masukan Elemen Array" << endl;
 	cout << "====================" << endl;
+
+	for (int i = 0; i < n; i++)                     // Menggunakan perulangan for untuk menyimpan
+	{
+		cout << "Data ke- " << (i + 1) << ": ";     // Memasukan atau menginputkan nilai data n
+		cin >> arr[i];                              // Menyimpan nilai data n kedalam array arr
+	}
+
+}
